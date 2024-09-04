7656d7bffb9764170f50f1e4bb5ebddd3a0e5c25
JuanCarrion <Juanpablocarriondiaz@gmail.com>
2024-09-04 00:23:48 -0500

    p15fixerrors

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
 const router = require("express").Router();
 const userRouter = require("./users");
 const itemsRouter = require("./clothingItems");
-const { documentNotFound } = require("../utils/errors");
 const { NotFoundError } = require("../middlewares/NotFoundError");
 
 router.use("/users", userRouter);
 
 router.use("/items", itemsRouter);
 
-router.use((req, res) => {
+router.use(() => {
   throw new NotFoundError("Router not found");
 });
 
